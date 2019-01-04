41dc907ee2216d856c018c5ccddd3e151f860663
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-04 00:27:34 +0000

    Implemented router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Search from './screens/Search';
+import Results from './screens/Results';
+import FourOhFour from './screens/FourOhFour';
 
 const App = () => (
-  <div>Hello World</div>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Search} />
+      <Route path="/results" component={Results} />
+      <Route component={FourOhFour} />
+    </Switch>
+  </Router>
 );
 
 export default App;
