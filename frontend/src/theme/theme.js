d1b0522bbbf02d6358b25bcf0c478083474489eb
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-04 04:15:31 +0000

    Added color palette

--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -1,8 +1,20 @@
 /**
+ * Color palette
+ */
+const palette = {
+  mirage: '#171c24',
+  dodgerBlue: '#1e90ff',
+  ghost: '#c2c7d1',
+  white: '#ffffff'
+};
+
+/**
  * Default theme
  */
 const theme = {
-  // ...
+  brand: {
+    color: palette.dodgerBlue
+  }
 };
 
 export default theme;
