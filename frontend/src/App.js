444336754607978dc4adf419d798196c8bd8f7b1
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-04 06:42:24 +0000

    Removed wrapper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import theme from './theme';
 
-import { Wrapper } from './styles';
 import Search from './screens/Search';
 import Results from './screens/Results';
 import FourOhFour from './screens/FourOhFour';
 
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Wrapper>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Search} />
-          <Route path="/results" component={Results} />
-          <Route component={FourOhFour} />
-        </Switch>
-      </Router>
-    </Wrapper>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route path="/results" component={Results} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </Router>
   </ThemeProvider>
 );
 
