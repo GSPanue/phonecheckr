3ba04d2380a7c103feee6c543b71058939e32135
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-04 04:18:14 +0000

    Added wrapper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,22 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import theme from './theme';
 
+import { Wrapper } from './styles';
 import Search from './screens/Search';
 import Results from './screens/Results';
 import FourOhFour from './screens/FourOhFour';
 
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Search} />
-        <Route path="/results" component={Results} />
-        <Route component={FourOhFour} />
-      </Switch>
-    </Router>
+    <Wrapper>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route path="/results" component={Results} />
+          <Route component={FourOhFour} />
+        </Switch>
+      </Router>
+    </Wrapper>
   </ThemeProvider>
 );
 
---
+++ frontend/src/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  max-width: 1140px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
+export {
+  Wrapper
+};
