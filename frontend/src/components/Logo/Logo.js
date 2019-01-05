1a3dd224a325bd04023efb2c732c053b589025c1
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-05 04:53:05 +0000

    Added Logo component

---
+++ frontend/src/components/Logo/Logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import definitions from './definitions';
+
+import { StyledSvgIcon } from './styles';
+
+const Logo = () => (
+  <StyledSvgIcon x="0px" y="0px" viewBox="0 0 369.5 42.7">
+    <g>
+      <path d={definitions.d1} />
+      <path d={definitions.d2} />
+      <path d={definitions.d3} />
+      <path d={definitions.d4} />
+      <path d={definitions.d5} />
+      <path d={definitions.d6} />
+      <path d={definitions.d7} />
+      <path d={definitions.d8} />
+      <path d={definitions.d9} />
+      <path d={definitions.d10} />
+      <path d={definitions.d11} />
+    </g>
+  </StyledSvgIcon>
+);
+
+export default Logo;
---
+++ frontend/src/components/Logo/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const StyledSvgIcon = styled(SvgIcon)`
+  && {
+    width: 138px;
+    height: 27px;
+    display: block;
+  }
+`;
+
+export {
+  StyledSvgIcon
+};
