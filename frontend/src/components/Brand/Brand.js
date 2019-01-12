bac0aeb7badcc48dde86ef2f9a0f9cf2e4241ea3
gspanue <gpanue@hotmail.com>
2019-01-12 06:17:58 +0000

    Passed props down to logo component

--- frontend/src/components/Brand/Brand.js
+++ frontend/src/components/Brand/Brand.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { StyledLink } from './styles';
 import Logo from '../Logo';
 
-const Brand = () => (
+const propTypes = {
+  large: PropTypes.bool
+};
+
+const defaultProps = {
+  large: undefined
+};
+
+const Brand = (props) => (
   <StyledLink to="/">
-    <Logo />
+    <Logo {...props} />
   </StyledLink>
 );
+
+Brand.propTypes = propTypes;
+Brand.defaultProps = defaultProps;
 
 export default Brand;
