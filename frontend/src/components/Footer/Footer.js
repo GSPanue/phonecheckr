090a4cddbb8753bc76cdb9dc0d9b2a344e2c6a54
gspanue <gpanue@hotmail.com>
2019-01-12 04:14:46 +0000

    Added Footer component

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Wrapper } from './styles';
+import SemicirclePattern from '../SemicirclePattern';
+
+const Footer = () => (
+  <Wrapper>
+    <SemicirclePattern />
+  </Wrapper>
+);
+
+export default Footer;
