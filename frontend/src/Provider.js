4c4f5e14e113c409b56949a0d2c7edfd758064a2
gspanue <gpanue@hotmail.com>
2019-01-12 01:40:41 +0000

    Added theme wrapper

---
+++ frontend/src/Provider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
+
+import { material, light } from './theme';
+
+const propTypes = {
+  children: PropTypes.element.isRequired
+};
+
+const Provider = ({ children }) => (
+  <MuiThemeProvider theme={material}>
+    <StyledComponentsThemeProvider theme={light}>
+      {children}
+    </StyledComponentsThemeProvider>
+  </MuiThemeProvider>
+);
+
+Provider.propTypes = propTypes;
+
+export default Provider;
