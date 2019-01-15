2ec67ecbc80162eb5ae794be9eaa0072bac126f9
gspanue <gpanue@hotmail.com>
2019-01-15 04:22:29 +0000

    Specified border radius

--- frontend/src/components/SearchBar/styles.js
+++ frontend/src/components/SearchBar/styles.js
@@ -9,6 +9,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   padding: 0 4px 0 13px;
+  border-radius: 2px;
   background: ${({ theme: { searchBar } }) => (searchBar.primaryBackground)};
 `;
 
