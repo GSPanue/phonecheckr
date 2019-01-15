fdba920e3eb452d0aecee4e06c3573b269a2ae2e
gspanue <gpanue@hotmail.com>
2019-01-15 03:24:53 +0000

    Imported SearchBar

--- frontend/src/screens/Search/Search.js
+++ frontend/src/screens/Search/Search.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Wrapper, Content } from './styles';
 import Brand from '../../components/Brand';
+import SearchBar from '../../components/SearchBar';
 import Footer from '../../components/Footer';
 
 const Search = () => (
   <Wrapper>
     <Content>
       <Brand large />
+      <SearchBar />
     </Content>
     <Footer />
   </Wrapper>
