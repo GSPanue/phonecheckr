5a167be1f57e46d3d7177d0c2ecefb6964758df2
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-20 23:59:26 +0000

    Set outline to none

--- frontend/src/components/Link/styles.js
+++ frontend/src/components/Link/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Wrapper = styled(Link)`
+  outline: none;
   text-decoration: none;
   user-select: none;
 `;
