780e6a3a0a0955299e0e28373edfc790a3b490b3
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-22 00:50:08 +0000

    Deleted Wrapper component and renamed Icon

--- frontend/src/components/SearchButton/styles.js
+++ frontend/src/components/SearchButton/styles.js
@@ -1,23 +1,8 @@
 import styled from 'styled-components';
 
-import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 
-const Wrapper = styled(IconButton)`
-  && {
-    padding: 6px;
-
-    ${({ theme: { ripple } }) => (`
-      color: ${ripple.primaryColor};
-
-      &:hover {
-        background-color: ${ripple.primaryColor}17;
-      }
-    `)}
-  }
-`;
-
-const Icon = styled(SearchIcon)`
+const StyledSearchIcon = styled(SearchIcon)`
   && {
     ${({ theme: { icon } }) => (`
       font-size: ${icon.fontSize};
@@ -27,6 +12,5 @@ const Icon = styled(SearchIcon)`
 `;
 
 export {
-  Wrapper,
-  Icon
+  StyledSearchIcon
 };
