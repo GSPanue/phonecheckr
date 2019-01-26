ba6161acae91a2f2dae5982db4ff06342cff0dae
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-26 11:44:52 +0000

    Moved helper functions to a seperate file and imported them

--- frontend/src/components/SuggestionItem/SuggestionItem.js
+++ frontend/src/components/SuggestionItem/SuggestionItem.js
@@ -1,68 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import match from 'autosuggest-highlight/match';
-import parse from 'autosuggest-highlight/parse';
-import shortid from 'shortid';
-import MenuItem from '@material-ui/core/MenuItem';
 
-import { StyledText } from './styles';
+import {
+  getParts,
+  renderChildren
+} from './helpers';
 
-/**
- * Calculates the part of the label to highlight based on the query and breaks the label into parts.
- *
- * @param {string} label - The label.
- * @param {string} query - The query.
- *
- * @returns {object}
- */
-const getParts = (label, query) => {
-  const matches = match(label, query);
-  const parts = parse(label, matches);
-
-  return (
-    parts
-  );
-};
-
-/**
- * Wraps each part of the label in a StyledText component.
- *
- * @param {objects} parts - The parts of a label.
- *
- * @returns {array}
- */
-const renderChildren = (parts) => (
-  parts.map(({ text, highlight: bold }) => {
-    const key = shortid.generate();
-
-    const textProps = {
-      key,
-      bold
-    };
-
-    return (
-      <StyledText {...textProps}>
-        {text}
-      </StyledText>
-    );
-  })
-);
+import { Wrapper } from './styles';
 
 const propTypes = {
   label: PropTypes.string.isRequired,
-  query: PropTypes.string.isRequired
+  query: PropTypes.string.isRequired,
+  isHighlighted: PropTypes.bool.isRequired
 };
 
-const SuggestionItem = ({ label, query }) => {
+const SuggestionItem = ({ label, query, isHighlighted }) => {
   const parts = getParts(label, query);
   const children = renderChildren(parts);
 
   return (
-    <MenuItem component="div">
+    <Wrapper component="div" selected={isHighlighted}>
       <div>
         {children}
       </div>
-    </MenuItem>
+    </Wrapper>
   );
 };
 
