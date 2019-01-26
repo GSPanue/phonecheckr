4963e4ef7939bc8ec1bf28ae0da4d32ec2f7eb8e
Gurdev S. Panue <gpanue@hotmail.com>
2019-01-26 11:45:37 +0000

    Styled a wrapper component

--- frontend/src/components/SuggestionItem/styles.js
+++ frontend/src/components/SuggestionItem/styles.js
@@ -1,9 +1,32 @@
+import React from 'react';
 import styled from 'styled-components';
+
+import MenuItem from '@material-ui/core/MenuItem';
+
+const Wrapper = styled((props) => (
+  <MenuItem classes={{ selected: 'selected' }} {...props} />
+))`
+  && {
+    ${({ theme: { suggestionItem, ripple } }) => (`
+      height: ${suggestionItem.height};
+      font-size: ${suggestionItem.fontSize};
+
+      &&:hover {
+        background-color: ${ripple.primaryColor}30;
+      }
+
+      &.selected {
+        background-color: ${ripple.primaryColor}5c;
+      }
+    `)}
+  }
+`;
 
 const StyledText = styled.span`
   ${({ bold, theme: { text } }) => (bold) && `font-weight: ${text.bold};`}
 `;
 
 export {
+  Wrapper,
   StyledText
 };
