ae84fad0851982d57647566a2cd65012f1752516
Gurdev S. Panue <gpanue@hotmail.com>
2019-02-09 19:02:26 +0000

    Displayed search query

--- frontend/src/screens/Results/Results.js
+++ frontend/src/screens/Results/Results.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { Wrapper, Content } from './styles';
+import { getQuery } from '../../helpers';
+
+import { Wrapper, Content, Heading, Query } from './styles';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-const Results = () => (
-  <Wrapper>
-    <Header />
-    <Content>
-      Placeholder
-    </Content>
-    <Footer />
-  </Wrapper>
-);
+const propTypes = {
+  location: PropTypes.object.isRequired
+};
+
+const Results = ({ location }) => {
+  const query = getQuery(location);
+
+  return (
+    <Wrapper>
+      <Header />
+      <Content>
+        <Heading>
+          You searched for&nbsp;
+          <Query>
+            &quot;
+            {query}
+            &quot;
+          </Query>
+        </Heading>
+      </Content>
+      <Footer />
+    </Wrapper>
+  );
+};
+
+Results.propTypes = propTypes;
 
 export default Results;
