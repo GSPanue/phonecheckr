58480814968b2add79a411b856d6725bc3b2522c
gspanue <gpanue@hotmail.com>
2019-05-11 20:08:06 +0100

    Removed Content component and changed Wrapper style

--- frontend/src/screens/Search/styles.js
+++ frontend/src/screens/Search/styles.js
@@ -2,25 +2,12 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  flex: 1;
-  align-items: center;
-`;
-
-const Content = styled.div`
-  display: flex;
   flex: 1;
   flex-direction: column;
-  align-self: stretch;
   align-items: center;
   justify-content: center;
-
-  & > * {
-    margin: 10px 0;
-  }
 `;
 
 export {
-  Wrapper,
-  Content
+  Wrapper
 };
