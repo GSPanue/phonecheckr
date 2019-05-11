f2a73f4067755d7e02f10a154a87b84545b11d43
gspanue <gpanue@hotmail.com>
2019-05-11 20:08:24 +0100

    Removed Content import

--- frontend/src/screens/Search/Search.js
+++ frontend/src/screens/Search/Search.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-import { Wrapper, Content } from './styles';
+import { Wrapper } from './styles';
 import Link from '../../components/Link';
 import Logo from '../../components/Logo';
 import SearchBar from '../../components/SearchBar';
 
 const Search = () => (
   <Wrapper>
-    <Content>
-      <Link to="/">
-        <Logo large />
-      </Link>
-      <SearchBar autoFocus autosuggest />
-    </Content>
+    <Link to="/">
+      <Logo large />
+    </Link>
+    <SearchBar autoFocus autosuggest />
   </Wrapper>
 );
 
