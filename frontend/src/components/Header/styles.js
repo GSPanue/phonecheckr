2397d909fc9f10bf03064cf9db0a2cfc5e1dfcae
gspanue <gpanue@hotmail.com>
2019-05-12 18:23:23 +0100

    Removed Wrapper style

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -2,19 +2,6 @@ import styled from 'styled-components';
 
 import SearchBar from '../SearchBar';
 
-const Wrapper = styled.header`
-  position: sticky;
-  height: 72px;
-  width: 100%;
-  top: 0;
-  left: auto;
-  right: 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  background: ${({ theme: { header } }) => (header.primaryBackground)};
-`;
-
 const StyledSearchBar = styled(SearchBar)`
   height: 42px;
   max-width: 344px;
@@ -31,6 +18,5 @@ const StyledSearchBar = styled(SearchBar)`
 `;
 
 export {
-  Wrapper,
   StyledSearchBar
 };
