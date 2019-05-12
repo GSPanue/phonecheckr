3a5564442ae54578b20c967c44e88ae75172988c
gspanue <gpanue@hotmail.com>
2019-05-12 18:33:24 +0100

    Added header border color and removed background color

--- frontend/src/theme/light/light.js
+++ frontend/src/theme/light/light.js
@@ -14,7 +14,7 @@ const palette = {
  */
 const light = {
   header: {
-    primaryBackground: palette.alabaster
+    primaryBorderColor: palette.alabaster
   },
   text: {
     primaryColor: palette.mineShaft,
