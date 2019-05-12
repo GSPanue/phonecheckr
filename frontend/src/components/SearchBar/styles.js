bf5a21ab12502c28e747046a5378a8c26e3d6ecc
gspanue <gpanue@hotmail.com>
2019-05-12 18:54:45 +0100

    Refactored wrapper styles

--- frontend/src/components/SearchBar/styles.js
+++ frontend/src/components/SearchBar/styles.js
@@ -12,14 +12,17 @@ const classNames = {
 const Wrapper = styled.div`
   position: relative;
   width: 100%;
-  max-width: ${({ theme: { searchBar } }) => (searchBar.width)};
-  height: ${({ theme: { searchBar } }) => (searchBar.height)};
   display: flex;
   align-items: center;
   padding: 0 5px 0 13px;
   border-radius: 2px;
   box-shadow: 0 2px 3px rgba(0,0,0,0.06);
-  background: ${({ theme: { searchBar } }) => (searchBar.primaryBackground)};
+
+  ${({ primary, theme: { searchBar } }) => (`
+    max-width: ${searchBar.width};
+    height: ${searchBar.height};
+    background: ${((primary) ? searchBar.primaryBackground : searchBar.secondaryBackground)};
+  `)}
 
   .${classNames.container} {
     display: flex;
