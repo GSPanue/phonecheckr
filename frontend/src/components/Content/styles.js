b0758341eee3596c88bb7be7de734b737ab03ff0
gspanue <gpanue@hotmail.com>
2019-05-16 20:44:55 +0100

    Added media query for margin-top

--- frontend/src/components/Content/styles.js
+++ frontend/src/components/Content/styles.js
@@ -3,11 +3,16 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   flex: 1;
   align-self: stretch;
-  margin-top: 57px;
+  margin-top: 56px;
   padding: 16px;
 
+  @media (min-width: 0px) and (orientation: landscape) {
+    margin-top: 48px;
+  }
+
   @media (min-width: 600px) {
-    margin-top: 65px;
+    margin-top: 64px;
+    padding: 24px;
   }
 `;
 
