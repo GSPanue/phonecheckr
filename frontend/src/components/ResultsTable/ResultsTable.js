92ce6dfd2118a2cb2c0639e803f68b6a54fc30c3
gspanue <gpanue@hotmail.com>
2019-05-17 19:36:26 +0100

    Added dummy row to table

--- frontend/src/components/ResultsTable/ResultsTable.js
+++ frontend/src/components/ResultsTable/ResultsTable.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 
-import { PrimaryTableCell } from './styles';
+import { PrimaryTableCell, SecondaryTableCell } from './styles';
 
 const ResultsTable = () => (
   <Table>
@@ -14,6 +15,13 @@ const ResultsTable = () => (
         <PrimaryTableCell align="center">Stores</PrimaryTableCell>
       </TableRow>
     </TableHead>
+    <TableBody>
+      <TableRow>
+        <SecondaryTableCell>iPhone 6s</SecondaryTableCell>
+        <SecondaryTableCell>£905.11</SecondaryTableCell>
+        <SecondaryTableCell align="center">23</SecondaryTableCell>
+      </TableRow>
+    </TableBody>
   </Table>
 );
 
--- frontend/src/components/ResultsTable/styles.js
+++ frontend/src/components/ResultsTable/styles.js
@@ -11,6 +11,17 @@ const PrimaryTableCell = styled(TableCell)`
   }
 `;
 
+const SecondaryTableCell = styled(TableCell)`
+  && {
+    font-size: 16px;
+    font-weight: ${({ theme: { text } }) => (text.light)};
+    padding: 0;
+    color: ${({ danger, theme: { table } }) => ((danger) ? table.secondaryColor : table.primaryColor)};
+    border-bottom: 1px solid ${({ theme: { table } }) => (table.primaryBorderColor)};
+  }
+`;
+
 export {
-  PrimaryTableCell
+  PrimaryTableCell,
+  SecondaryTableCell
 };
