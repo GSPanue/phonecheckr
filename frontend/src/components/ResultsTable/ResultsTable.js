fadd58393bf8ff0dddf2fb9bb2e91f3d2f2a7d12
Gurdev S. Panue <gpanue@hotmail.com>
2019-06-20 14:34:09 +0100

    Implemented table pagination

--- frontend/src/components/ResultsTable/ResultsTable.js
+++ frontend/src/components/ResultsTable/ResultsTable.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 
 import { getTableItems } from './helpers';
 
 import { StyledTableCell } from './styles';
+import TablePaginationActions from '../TablePaginationActions';
 
 const propTypes = {
   items: PropTypes.array.isRequired
 };
 
 const ResultsTable = ({ items }) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    const { target: { value } } = event;
+
+    setRowsPerPage(parseInt(value, 10));
+  };
+
   const tableItems = getTableItems(items);
 
   return (
@@ -28,6 +44,20 @@ const ResultsTable = ({ items }) => {
       <TableBody>
         {tableItems}
       </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TablePagination
+            rowsPerPageOptions={[10, 25]}
+            colSpan={3}
+            count={tableItems.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+            ActionsComponent={TablePaginationActions}
+          />
+        </TableRow>
+      </TableFooter>
     </Table>
   );
 };
