d1f102729379f135f707f4d91755bdf4610089bb
Gurdev S. Panue <gpanue@hotmail.com>
2019-06-21 15:24:38 +0100

    Implemented useEffect and conditional render

--- frontend/src/screens/Product/Product.js
+++ frontend/src/screens/Product/Product.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 import { Wrapper } from './styles';
 import Header from '../../components/Header';
+import ProgressBar from '../../components/ProgressBar';
 import Content from '../../components/Content';
 
-const Product = () => (
-  <Wrapper>
-    <Header />
-    <Content>
-      Product
-    </Content>
-  </Wrapper>
-);
+const propTypes = {
+  show: PropTypes.bool.isRequired,
+  fetchProduct: PropTypes.func.isRequired,
+  hideProduct: PropTypes.func.isRequired
+};
+
+const Product = ({ show, fetchProduct, hideProduct }) => {
+  useEffect(() => {
+    if (!show) {
+      fetchProduct();
+    }
+
+    return (() => {
+      if (show) {
+        hideProduct();
+      }
+    });
+  }, [show]);
+
+  if (show) {
+    return (
+      <Wrapper>
+        <Header />
+        <Content>
+          Product
+        </Content>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <Header />
+      <ProgressBar />
+    </Wrapper>
+  );
+};
+
+Product.propTypes = propTypes;
 
 export default Product;
--- frontend/src/screens/Product/index.js
+++ frontend/src/screens/Product/index.js
@@ -1,1 +1,23 @@
-export { default } from './Product';
+import { connect } from 'react-redux';
+
+import {
+  fetchProduct,
+  hideProduct
+} from '../../actions';
+
+import Product from './Product';
+
+const mapStateToProps = ({ product }) => {
+  const { show } = product;
+
+  return ({
+    show
+  });
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchProduct: () => dispatch(fetchProduct()),
+  hideProduct: () => dispatch(hideProduct())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
