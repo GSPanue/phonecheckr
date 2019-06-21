74f002983556e4dc87e27bcc758a2de390b88212
Gurdev S. Panue <gpanue@hotmail.com>
2019-06-21 15:16:59 +0100

    Partially implemented fetchProduct

--- frontend/src/actions/product/product.js
+++ frontend/src/actions/product/product.js
@@ -1,17 +1,43 @@
 import {
+  SHOW_PROGRESS_BAR,
+  HIDE_PROGRESS_BAR,
   SHOW_PRODUCT,
   HIDE_PRODUCT
 } from '../../constants';
 
-const showProduct = () => ({
-  type: SHOW_PRODUCT
-});
+/**
+ * @todo Add param for product
+ */
+const fetchProduct = () => (
+  (dispatch) => {
+    dispatch({
+      type: SHOW_PROGRESS_BAR
+    });
+
+    /**
+     * @todo Fetch and store product information
+     */
+
+    const productInformation = null;
+
+    setTimeout(() => {
+      dispatch({
+        type: HIDE_PROGRESS_BAR
+      });
+
+      dispatch({
+        type: SHOW_PRODUCT,
+        payload: productInformation
+      });
+    }, 500);
+  }
+);
 
 const hideProduct = () => ({
   type: HIDE_PRODUCT
 });
 
 export {
-  showProduct,
+  fetchProduct,
   hideProduct
 };
