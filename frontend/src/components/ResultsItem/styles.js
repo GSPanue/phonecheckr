16a69101022f9b49e0caf2279dec35c27bbc804e
Gurdev S. Panue <gpanue@hotmail.com>
2019-06-23 20:04:03 +0100

    Changed padding

--- frontend/src/components/ResultsItem/styles.js
+++ frontend/src/components/ResultsItem/styles.js
@@ -11,7 +11,7 @@ const StyledTableCell = styled(TableCell)`
   && {
     font-size: 16px;
     font-weight: ${({ theme: { text } }) => (text.light)};
-    padding: 0;
+    padding-left: 0;
     color: ${({ danger, theme: { table } }) => ((danger) ? table.secondaryColor : table.primaryColor)};
     border-bottom: 1px solid ${({ theme: { table } }) => (table.primaryBorderColor)};
   }
--- frontend/src/components/ResultsTable/styles.js
+++ frontend/src/components/ResultsTable/styles.js
@@ -5,7 +5,7 @@ import TableCell from '@material-ui/core/TableCell';
 const StyledTableCell = styled(TableCell)`
   && {
     font-size: 16px;
-    padding: 0;
+    padding-left: 0;
     color: ${({ theme: { table } }) => (table.primaryColor)};
     border-bottom: 1px solid ${({ theme: { table } }) => (table.primaryBorderColor)};
   }
