688ae3c2954c5e418117d3b3f9334617ce1b1889
Gurdev S. Panue <gpanue@hotmail.com>
2019-06-25 16:55:28 +0100

    Added margin to back button

--- frontend/src/components/BackButton/styles.js
+++ frontend/src/components/BackButton/styles.js
@@ -4,6 +4,8 @@ import Button from '@material-ui/core/Button';
 
 const StyledButton = styled(Button)`
   && {
+    margin-bottom: 28px;
+
     ${({ theme: { button } }) => (`
     border: 1px solid ${button.primaryBorderColor};
     color: ${button.primaryColor};
