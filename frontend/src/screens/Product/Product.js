ab848a0aae10bdeb1e192182e3830ae89babd299
Gurdev S. Panue <gpanue@hotmail.com>
2019-07-05 18:06:31 +0100

    Added inner content

--- frontend/src/screens/Product/Product.js
+++ frontend/src/screens/Product/Product.js
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import { Wrapper } from './styles';
+import {
+  Wrapper,
+  InnerContent,
+  TextContainer,
+  Title,
+  Description
+} from './styles';
 import Header from '../../components/Header';
 import ProgressBar from '../../components/ProgressBar';
 import Content from '../../components/Content';
 import BackButton from '../../components/BackButton';
+import Range from '../../components/Range';
+import StorageDropdown from '../../components/StorageDropdown';
 
 const propTypes = {
   show: PropTypes.bool.isRequired,
@@ -14,6 +22,9 @@ const propTypes = {
   hideProduct: PropTypes.func.isRequired
 };
 
+/**
+ * @todo Remove placeholder text
+ */
 const Product = ({
   show,
   showBackButton,
@@ -38,7 +49,15 @@ const Product = ({
         <Header />
         <Content>
           {(showBackButton) && <BackButton />}
-          Product
+          <InnerContent>
+            <img height="300" width="300" src="https://images.pricerunner.com/product/600x600/1599071093/Apple-iPhone-6S-32GB.jpg?c=0.7" alt="d" />
+            <TextContainer>
+              <Title>Apple iPhone 6s</Title>
+              <Description>Apple iOS, 4.7&quot;, Release Year 2016, 12 MP, 143g, 4G LTE, 3G, HSPA, EDGE, GPRS, Wi-Fi, NFC, Bluetooth, 2G, VoLTE, DC-HSPA, HSPA+</Description>
+              <Range from="239.99" to="460.24" />
+              <StorageDropdown storageOptions={['16GB', '32GB']} />
+            </TextContainer>
+          </InnerContent>
         </Content>
       </Wrapper>
     );
