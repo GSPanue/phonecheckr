db3e53cad3c395391ea91c3e50b81a2e4dabbd20
Gurdev S. Panue <gpanue@hotmail.com>
2019-08-27 17:29:44 +0100

    Added 404 screen

--- frontend/src/screens/FourOhFour/FourOhFour.js
+++ frontend/src/screens/FourOhFour/FourOhFour.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import {
+  Wrapper,
+  TextContainer,
+  StyledText
+} from './styles';
+import Header from '../../components/Header';
+import Content from '../../components/Content';
+
 const FourOhFour = () => (
-  <div>404</div>
+  <Wrapper>
+    <Header />
+    <Content>
+      <TextContainer column center>
+        <StyledText large danger>404</StyledText>
+        <StyledText small>The requested URL was not found on this server.</StyledText>
+      </TextContainer>
+    </Content>
+  </Wrapper>
 );
 
 export default FourOhFour;
---
+++ frontend/src/screens/FourOhFour/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: ${({ column }) => ((column) ? 'column' : 'row')};
+  align-items: center;
+
+  ${({ padding }) => (padding) && (`
+    padding-top: 6px;
+  `)}
+
+  ${({ center }) => (center) && (`
+    height: 100%;
+    justify-content: center;
+  `)}
+`;
+
+const StyledText = styled.span`
+  font-size: ${({ large, small }) => {
+    if (large || small) {
+      return (
+        (large) ? '32px' : '16px'
+      );
+    }
+
+    return (
+      '24px'
+    );
+  }};
+
+  font-weight: ${({ theme: { text } }) => (text.medium)};
+
+  color: ${({ danger, theme: { text } }) => (
+    (danger) ? text.secondaryColor : text.primaryColor
+  )};
+`;
+
+export {
+  Wrapper,
+  TextContainer,
+  StyledText
+};
