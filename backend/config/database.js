ac2d5b2347484919d1e12d6daf0d6cdb526390f0
Gurdev S. Panue <gpanue@hotmail.com>
2019-08-30 14:58:07 +0100

    Added database config file

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+import { env } from '../src/helpers';
+
+const config = {
+  client: 'mysql',
+  connection: {
+    host: env('DB_HOST'),
+    port: env('DB_PORT'),
+    user: env('DB_USERNAME'),
+    password: env('DB_PASSWORD'),
+    database: env('DB_DATABASE')
+  }
+};
+
+export default config;
