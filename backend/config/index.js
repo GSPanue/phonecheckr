2ca9c7bf37ffe0ac41b577d8f7e4a2846fef3212
Gurdev S. Panue <gpanue@hotmail.com>
2019-08-30 15:47:16 +0100

    Created index file for config

---
+++ backend/config/index.js
@@ -0,0 +1,9 @@
+import database from './database';
+import webpackDevConfig from './webpack.dev.config.babel';
+import webpackProdConfig from './webpack.prod.config.babel';
+
+export {
+  database,
+  webpackDevConfig,
+  webpackProdConfig
+};
