ec479afab5d382da1c27372124ae43abe5057934
Gurdev S. Panue <gpanue@hotmail.com>
2019-09-08 16:38:02 +0100

    Removed webpack config files

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,9 +1,5 @@
 import database from './database';
-import webpackDevConfig from './webpack.dev.config.babel';
-import webpackProdConfig from './webpack.prod.config.babel';
 
 export {
-  database,
-  webpackDevConfig,
-  webpackProdConfig
+  database
 };
