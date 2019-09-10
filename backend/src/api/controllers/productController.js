cfd6058784ad80d64c938a9b8bfb6038583c51c4
Gurdev S. Panue <gpanue@hotmail.com>
2019-09-10 19:27:50 +0100

    Implemented product API

--- backend/src/api/controllers/productController.js
+++ backend/src/api/controllers/productController.js
@@ -1,10 +1,57 @@
+import Product from '@/database/models/Product';
+
 const getProduct = (req, res) => {
-  /**
-   * @todo Fetch data from database
-   */
-  res.send({
-    product: {}
-  });
+  const { query: { id } } = req;
+
+  Product.query()
+    .joinEager({
+      urls: true,
+      pages: true,
+      brands: true,
+      models: {
+        colours: true,
+        storage_capacities: true
+      },
+      suppliers: true
+    })
+    .where('pages.name', id)
+    .omit([
+      'id',
+      'url_id',
+      'page_id',
+      'brand_id',
+      'model_id',
+      'supplier_id',
+      'colour_id',
+      'storage_capacity_id',
+      'pages'
+    ])
+    .orderBy('price')
+    .then((data) => {
+      const lowestPrice = data[0].price;
+      const highestPrice = data[data.length - 1].price;
+
+      const {
+        description,
+        image,
+        brands: { name: brand },
+        models: { name: model },
+      } = data[0];
+
+      const product = {
+        name: `${brand} ${model}`,
+        description,
+        image,
+        range: [lowestPrice, highestPrice],
+        deals: {
+          ...data
+        }
+      };
+
+      res.send({
+        product
+      });
+    });
 };
 
 export {
