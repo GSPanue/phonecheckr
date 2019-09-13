9b03ec6c71f05aa38d981ac0872db5f42c59fcf6
Gurdev S. Panue <gpanue@hotmail.com>
2019-09-13 17:13:47 +0100

    Added helper for mounting with router

--- frontend/src/helpers/test/enzyme/enzyme.js
+++ frontend/src/helpers/test/enzyme/enzyme.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { mount } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 /**
- * Mounts and returns a themed component.
+ * Wraps children with a ThemeProvider component.
  *
- * @param {array} children - The child components.
+ * @param {array} children - The children.
  * @param {object} theme - The theme.
+ *
+ * @returns {object}
  */
 const mountWithTheme = (children, theme) => (
   mount(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
 );
 
+/**
+ * Wraps children with a BrowserRouter component.
+ *
+ * @param {array} children - The children.
+ *
+ * @returns {object}
+ */
+const mountWithRouter = (children) => (
+  mount(<Router>{children}</Router>)
+);
+
 export {
-  mountWithTheme
+  mountWithTheme,
+  mountWithRouter
 };
