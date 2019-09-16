f6f565540290591e076538ff47ac27396eda6bb5
Gurdev S. Panue <gpanue@hotmail.com>
2019-09-16 16:53:48 +0100

    Removed indent from imports

--- scraper/src/main/java/com/phonecheckr/app/model/Colour.java
+++ scraper/src/main/java/com/phonecheckr/app/model/Colour.java
@@ -1,13 +1,13 @@
 package com.phonecheckr.app.model;
 
-    import java.io.Serializable;
+import java.io.Serializable;
 
-    import javax.persistence.Id;
-    import javax.persistence.GeneratedValue;
-    import javax.persistence.GenerationType;
-    import javax.persistence.Column;
-    import javax.persistence.Entity;
-    import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * Entity class Colour
