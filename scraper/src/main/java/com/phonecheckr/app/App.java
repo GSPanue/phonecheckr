ffc52e78999309186a6e57067292d36c00ef2fde
Gurdev S. Panue <gpanue@hotmail.com>
2019-09-16 21:54:30 +0100

    Added JavaDoc comment to class

--- scraper/src/main/java/com/phonecheckr/app/App.java
+++ scraper/src/main/java/com/phonecheckr/app/App.java
@@ -3,6 +3,11 @@ package com.phonecheckr.app;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * Main class App
+ *
+ * @author Gurdev S. Panue
+ */
 public class App {
   public static void main(String[] args) {
     // Load configuration class
