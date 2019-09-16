5370131473acbf96cddab26e159ac5f68c056eb0
Gurdev S. Panue <gpanue@hotmail.com>
2019-09-16 21:52:05 +0100

    Loaded configuration class

--- scraper/src/main/java/com/phonecheckr/app/App.java
+++ scraper/src/main/java/com/phonecheckr/app/App.java
@@ -1,7 +1,11 @@
 package com.phonecheckr.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
-    public static void main(String[] args) {
-        // Do something...
-    }
+  public static void main(String[] args) {
+    // Load configuration class
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+  }
 }
