f485aa5d8428353f0b810c57119bf7ea673f9bb9
Gurdev S. Panue <gpanue@hotmail.com>
2019-09-22 16:11:53 +0100

    Added Currys scraper bean

--- scraper/src/main/java/com/phonecheckr/app/AppConfig.java
+++ scraper/src/main/java/com/phonecheckr/app/AppConfig.java
@@ -28,8 +28,10 @@ public class AppConfig {
    */
   @Bean
   public void startScraping() {
+    System.out.println("Enter `stop` to stop scraping.");
+
     Scanner scanner = new Scanner(System.in);
-    CurrysScraper currysScraper = new CurrysScraper();
+    CurrysScraper currysScraper = createCurrysScraper();
 
     // Start scraper(s)
     currysScraper.start();
@@ -41,7 +43,34 @@ public class AppConfig {
       input = scanner.nextLine();
     }
 
+    System.out.println("Stopping the scrapers...");
+
     // Close the session factory
     Hibernate.destroySessionFactory();
+  }
+
+  /**
+   * Creates the Currys scraper.
+   *
+   * @return the Currys scraper.
+   */
+  @Bean
+  public CurrysScraper createCurrysScraper() {
+    CurrysScraper currysScraper = new CurrysScraper();
+
+    currysScraper.setSupplier("Currys");
+    currysScraper.setSearchPage("https://www.currys.co.uk/gbuk/phones-broadband-and-sat-nav/mobile-phones-and-accessories/mobile-phones/362_3412_32041_xx_xx/1_50/brand-asc/xx-criteria.html");
+    currysScraper.setProductSelector("article.product");
+    currysScraper.setUrlSelector("header.productTitle > a");
+    currysScraper.setBrandSelector("header.productTitle > a > span[data-product=brand]");
+    currysScraper.setModelSelector("header.productTitle > a > span[data-product=name]");
+    currysScraper.setColourSelector("header.productTitle > a > span[data-product=name]");
+    currysScraper.setStorageCapacitySelector("header.productTitle > a > span[data-product=name]");
+    currysScraper.setImageSelector("div.swiper-slide > a");
+    currysScraper.setDescriptionSelector("ul.productDescription > li");
+    currysScraper.setPriceSelector("strong.price");
+    currysScraper.setNextPageSelector("ul.pagination > li > a[title=next]");
+
+    return currysScraper;
   }
 }
